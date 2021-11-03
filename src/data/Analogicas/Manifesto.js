9ae9048cec669f78d59c854baeb5e6cda7521415
Matheus Gonçalves Rocha <matheus98rocha@gmail.com>
2021-11-02 22:07:11 -0300

    corrections

--- src/data/Analogicas/Manifesto.js
+++ src/data/Analogicas/Manifesto.js
@@ -1,23 +1,21 @@
-import photo1 from '../../assets/images/analogicas/Manifesto/1.JPG';
-import photo2 from '../../assets/images/analogicas/Manifesto/2.PNG';
-import photo3 from '../../assets/images/analogicas/Manifesto/3.jpg';
-import photo4 from '../../assets/images/analogicas/Manifesto/4.JPG';
-import photo5 from '../../assets/images/analogicas/Manifesto/5.JPG';
-import photo6 from '../../assets/images/analogicas/Manifesto/6.JPG';
-import photo7 from '../../assets/images/analogicas/Manifesto/7.JPG';
-import photo8 from '../../assets/images/analogicas/Manifesto/8.JPG';
-import photo9 from '../../assets/images/analogicas/Manifesto/9.JPG';
-import photo10 from '../../assets/images/analogicas/Manifesto/10.JPG';
-import photo11 from '../../assets/images/analogicas/Manifesto/11.JPG';
-import photo12 from '../../assets/images/analogicas/Manifesto/12.JPG';
-import photo13 from '../../assets/images/analogicas/Manifesto/13.JPG';
-import photo14 from '../../assets/images/analogicas/Manifesto/14.PNG';
-import photo15 from '../../assets/images/analogicas/Manifesto/15.png';
-import photo16 from '../../assets/images/analogicas/Manifesto/16.jpg';
-import photo17 from '../../assets/images/analogicas/Manifesto/17.jpg';
-import photo18 from '../../assets/images/analogicas/Manifesto/18.JPG';
-// import photo19 from '../../assets/images/analogicas/Manifesto/19.JPG';
-
+import photo1 from '../../assets/images/analogicas/Manifesto/1.jpg';
+import photo2 from '../../assets/images/analogicas/Manifesto/2_Easy-Resize.com.jpg';
+import photo3 from '../../assets/images/analogicas/Manifesto/3_Easy-Resize.com.jpg';
+import photo4 from '../../assets/images/analogicas/Manifesto/4_Easy-Resize.com.jpg';
+import photo5 from '../../assets/images/analogicas/Manifesto/5_Easy-Resize.com.jpg';
+import photo6 from '../../assets/images/analogicas/Manifesto/6_Easy-Resize.com.jpg';
+import photo7 from '../../assets/images/analogicas/Manifesto/7_Easy-Resize.com.jpg';
+import photo8 from '../../assets/images/analogicas/Manifesto/8_Easy-Resize.com.jpg';
+import photo9 from '../../assets/images/analogicas/Manifesto/9_Easy-Resize.com.jpg';
+import photo10 from '../../assets/images/analogicas/Manifesto/10_Easy-Resize.com.jpg';
+import photo11 from '../../assets/images/analogicas/Manifesto/11_Easy-Resize.com.jpg';
+import photo12 from '../../assets/images/analogicas/Manifesto/12_Easy-Resize.com.jpg';
+import photo13 from '../../assets/images/analogicas/Manifesto/13_Easy-Resize.com.jpg';
+import photo14 from '../../assets/images/analogicas/Manifesto/14_Easy-Resize.com.jpg';
+import photo15 from '../../assets/images/analogicas/Manifesto/15_Easy-Resize.com.jpg';
+import photo16 from '../../assets/images/analogicas/Manifesto/16_Easy-Resize.com.jpg';
+import photo17 from '../../assets/images/analogicas/Manifesto/17_Easy-Resize.com.jpg';
+import photo18 from '../../assets/images/analogicas/Manifesto/18_Easy-Resize.com.jpg';
 
 export const Manifesto = [
     {

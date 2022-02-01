4fd3ffe0af9449f44c31dafd7ddd60940d000a32
matheus98rocha <matheus98rocha@gmail.com>
2022-01-31 22:25:51 -0300

    Corrections on photos and cards

--- src/data/session/Session.js
+++ src/data/session/Session.js
@@ -17,17 +17,20 @@ export const Session = [
         imageUrl: photo2,
         body: "Imanges retiradas em Bauru - SP . Andrews Bronca , ator , dançarino e coreógrafo, MC , artista independente . @oficialbronca",
         destiny: "/sessao/bronca",
-    }, {
-        title: "Editora",
-        imageUrl: photo3,
-        body: "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        destiny: "/sessao/editora",
-    }, {
-        title: "Iza",
-        imageUrl: photo4,
-        body: "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        destiny: "/sessao/iza",
-    }, {
+    },
+    // {
+    //     title: "Editora",
+    //     imageUrl: photo3,
+    //     body: "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    //     destiny: "/sessao/editora",
+    // },
+    // {
+    //     title: "Iza",
+    //     imageUrl: photo4,
+    //     body: "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    //     destiny: "/sessao/iza",
+    // },
+    {
         title: "Sol",
         imageUrl: photo5,
         body: " Imagens retiradas no Jarezinho - Rj, Sol e sua mãe Raissa.",
@@ -35,7 +38,7 @@ export const Session = [
     }, {
         title: "Wakanda",
         imageUrl: photo6,
-        body: "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        body: "Imagens retiradas no Jacarezinho - Rj , Wakanda Chuki Brechozeira e Styling e Saúde.",
         destiny: "/sessao/wakanda",
     }
 ]
--- src/data/session/wakanda.js
+++ src/data/session/wakanda.js
@@ -1,17 +1,17 @@
 import photo1 from '../../assets/images/sessao/wakanda/1.jpg';
-import photo2 from '../../assets/images/sessao/wakanda/2.jpg';
+// import photo2 from '../../assets/images/sessao/wakanda/2.jpg';
 import photo3 from '../../assets/images/sessao/wakanda/3.jpg';
-import photo4 from '../../assets/images/sessao/wakanda/4.jpg';
+// import photo4 from '../../assets/images/sessao/wakanda/4.jpg';
 import photo5 from '../../assets/images/sessao/wakanda/5.jpg';
 import photo6 from '../../assets/images/sessao/wakanda/6.jpg';
 import photo7 from '../../assets/images/sessao/wakanda/7.jpg';
-import photo8 from '../../assets/images/sessao/wakanda/8.jpg';
-import photo9 from '../../assets/images/sessao/wakanda/9.jpg';
+// import photo8 from '../../assets/images/sessao/wakanda/8.jpg';
+// import photo9 from '../../assets/images/sessao/wakanda/9.jpg';
 import photo10 from '../../assets/images/sessao/wakanda/10.jpg';
 import photo11 from '../../assets/images/sessao/wakanda/11.jpg';
 import photo12 from '../../assets/images/sessao/wakanda/12.jpg';
 import photo13 from '../../assets/images/sessao/wakanda/13.jpg';
-import photo14 from '../../assets/images/sessao/wakanda/14.jpg';
+// import photo14 from '../../assets/images/sessao/wakanda/14.jpg';
 import photo15 from '../../assets/images/sessao/wakanda/15.jpg';
 
 
@@ -22,15 +22,15 @@ export const wakanda = [
     {
         image: photo1
     },
-    {
-        image: photo2
-    },
+    // {
+    //     image: photo2
+    // },
     {
         image: photo3
     },
-    {
-        image: photo4
-    },
+    // {
+    //     image: photo4
+    // },
     {
         image: photo5
     },
@@ -41,15 +41,14 @@ export const wakanda = [
 
         image: photo7
     },
+    // {
 
-    {
+    //     image: photo8
+    // },
+    // {
 
-        image: photo8
-    },
-    {
-
-        image: photo9
-    },
+    //     image: photo9
+    // },
     {
 
         image: photo10
@@ -66,10 +65,10 @@ export const wakanda = [
 
         image: photo13
     },
-    {
+    // {
 
-        image: photo14
-    },
+    //     image: photo14
+    // },
     {
 
         image: photo15
